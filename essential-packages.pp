cb042a5f1b24aab2c55908e6701b294fd144f9d0
Lyon Liang <lyon.liang12@gmail.com>
2016-09-23 11:44:56 +0800

    install git/zsh/curl/oh-my-zsh/vim

--- essential-packages.pp
+++ essential-packages.pp
@@ -1,8 +1,16 @@
 package { 'vim': ensure => present }
 package { 'curl': ensure => present }
 package { 'zsh': ensure => present }
+package { 'git': ensure => present }
 
 exec { 'oh-my-zsh':
-  command => 'curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sudo sh',
-  require => [Package['zsh'], Package['curl']]
+  path => '/usr/bin:/usr/sbin:/bin',
+  command => 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"',
+  require => [Package['zsh'], Package['curl'], Package['git']]
+}
+
+# to avoid bad ssh pipe when using ssh login
+exec { 'ssh-alive-interval':
+  path => '/usr/bin:/usr/sbin:/bin',
+  command => 'echo "ClientAliveInterval 30" >> /etc/ssh/sshd_config'
 }
